0b85cef633f42c3b56d32f1368ad39818ba02181
Jason Bailey <jmbailey816@gmail.com>
2021-08-18 13:13:27 -0400

    models added

---
+++ models/workout.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "enter exercise type",
+      },
+
+      name: {
+        type: String,
+        trim: true,
+        required: "enter exercise name",
+      },
+
+      // from exercise.html - Duration (minutes)
+      duration: {
+        type: Number,
+        required: "enter exercise duration in minutes",
+      },
+
+      // from exercise.html - Weight (lbs)
+      weight: {
+        type: Number,
+      },
+
+      // from exercise.html - Reps
+      reps: {
+        type: Number,
+      },
+
+      // from exercise.html - Sets
+      sets: {
+        type: Number,
+      },
+
+      // from exercise.html - Distance (miles)
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
