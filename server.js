a2ce1945da0ed91b97bafedaf8de627466431ac6
Jason Bailey <jmbailey816@gmail.com>
2021-08-19 13:36:40 -0400

    Process.env.port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
